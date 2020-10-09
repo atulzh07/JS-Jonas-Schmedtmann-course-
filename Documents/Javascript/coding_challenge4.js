4bab386891d730d96c1600317f97ff3cb6d3f2d9
atulzh07 <atulzh7@protonmail.com>
2020-10-09 22:39:47 +0545

    Refactored and used get & this function

--- Documents/Javascript/coding_challenge4.js
+++ Documents/Javascript/coding_challenge4.js
@@ -1,32 +1,28 @@
-var john = {
+function bmi(mass,height){
+    return mass/(height * height);
+}
+let john = {
     name: 'John Meller',
-    mass: 60,
-    height: 1.88,
-    bmi: function(){
-        return this.mass/(this.height * this.height);
+    mass: 80,
+    height: 1.79,
+    get w(){
+        return bmi(this.mass, this.height);
     }
 }
-
-var mark = {
+let mark = {
     name: 'Mark Brand',
     mass: 60,
     height: 1.88,
-    bmi: function(){
-        return this.mass/(this.height * this.height);
+    get w(){
+        return bmi(this.mass, this.height);
     }
 }
-
-console.log('John\'s BMI is: ' + john.bmi());
-console.log('Mark\'s BMI is: ' + mark.bmi());
-
-if (john.bmi() > mark.bmi()) {
+console.log('John\'s BMI is: ' + john.w);
+console.log('Mark\'s BMI is: ' + mark.w);
+if (john.w > mark.w) {
     console.log('John has a higher body mass index');
-}
-
-else if (john.bmi() < mark.bmi()) {
+} else if (john.w < mark.w) {
     console.log('Mark has a higher body mass index');
-}
-
-else {
+} else {
     console.log('Both John and Mark have the same body mass index');
 }
